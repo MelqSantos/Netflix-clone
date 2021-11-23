3a5db415c63367faa473c4105d6e380fc567368c
Melqui Santos <melqsantos96@gmail.com>
2021-11-23 20:28:07 -0300

    Criação de novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './shared/full-banner/full-banner.component';
+import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MoviesComponent } from './shared/movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FullBannerComponent,
+    ListTitlesComponent,
+    FooterComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
